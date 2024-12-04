31a40389cff382502dd17e68e56827f1c82b7056
Rene De La Garza <rene.delagarza@gmail.com>
2024-12-03 21:14:54 -0600

    refactor day03 to have just one func

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,56 +1,4 @@
-pub fn solve_day_03_01(input: &str) -> String {
-    let mut current_idx: usize = 0;
-    let mut in_mul: bool = false;
-    let mut curr_value_idx: usize = 0;
-    let mut values: Vec<String> = vec!["".into(), "".into()];
-    let mut sum: u64 = 0;
-    while current_idx < input.len() {
-        let c = input.chars().nth(current_idx).unwrap();
-        match c {
-            'm' => {
-                let is_u = input.chars().nth(current_idx + 1).unwrap();
-                let is_l = input.chars().nth(current_idx + 2).unwrap();
-                let is_parenthisis = input.chars().nth(current_idx + 3).unwrap();
-                if is_u == 'u' && is_l == 'l' && is_parenthisis == '(' {
-                    in_mul = true;
-                    current_idx += 4;
-                } else {
-                    current_idx += 1;
-                }
-            }
-            _ => {
-                if in_mul {
-                    if c.is_digit(10) {
-                        values[curr_value_idx].push(c);
-                    } else if c == ',' {
-                        if curr_value_idx == 1 {
-                            in_mul = false;
-                            curr_value_idx = 0;
-                            values = vec!["".into(), "".into()];
-                        } else {
-                            curr_value_idx += 1;
-                        }
-                    } else if c == ')' {
-                        curr_value_idx = 0;
-                        let first: u64 = values[0].parse().unwrap_or(0);
-                        let second: u64 = values[1].parse().unwrap_or(0);
-                        sum += first * second;
-                        values = vec!["".into(), "".into()];
-                        in_mul = false;
-                    } else {
-                        curr_value_idx = 0;
-                        values = vec!["".into(), "".into()];
-                        in_mul = false;
-                    }
-                }
-                current_idx += 1;
-            }
-        }
-    }
-    sum.to_string()
-}
-
-pub fn solve_day_03_02(input: &str) -> String {
+pub fn solve_day_03(input: &str, allow_enable: bool) -> String {
     let mut current_idx: usize = 0;
     let mut in_mul: bool = false;
     let mut curr_value_idx: usize = 0;
@@ -60,7 +8,7 @@ pub fn solve_day_03_02(input: &str) -> String {
     while current_idx < input.len() {
         let c = input.chars().nth(current_idx).unwrap();
         match c {
-            'd' => {
+            'd' if allow_enable => {
                 let is_o = input.chars().nth(current_idx + 1).unwrap();
                 let is_n = input.chars().nth(current_idx + 2).unwrap();
                 let is_quote = input.chars().nth(current_idx + 3).unwrap();
@@ -102,7 +50,6 @@ pub fn solve_day_03_02(input: &str) -> String {
                     } else if c == ')' {
                         curr_value_idx = 0;
                         if enable_mul {
-                            println!("{:?}", values);
                             let first: u64 = values[0].parse().unwrap_or(0);
                             let second: u64 = values[1].parse().unwrap_or(0);
                             sum += first * second;
@@ -124,8 +71,8 @@ pub fn solve_day_03_02(input: &str) -> String {
 
 fn main() {
     let input = include_str!("./input.txt");
-    println!("Day 03.01 {:?}", solve_day_03_01(input));
-    println!("Day 03.02 {:?}", solve_day_03_02(input));
+    println!("Day 03.01 {:?}", solve_day_03(input, false));
+    println!("Day 03.02 {:?}", solve_day_03(input, true));
 }
 
 #[cfg(test)]
@@ -135,16 +82,21 @@ mod tests {
     #[test]
     fn day_03_01_sample() {
         let input = include_str!("./sample.txt");
-        assert_eq!(solve_day_03_01(input), "161");
+        assert_eq!(solve_day_03(input, false), "161");
     }
     #[test]
     fn day_03_02_sample() {
         let input = include_str!("./sample.txt");
-        assert_eq!(solve_day_03_02(input), "48");
+        assert_eq!(solve_day_03(input, true), "48");
     }
     #[test]
     fn day_03_01_input() {
         let input = include_str!("./input.txt");
-        assert_eq!(solve_day_03_01(input), "188741603");
+        assert_eq!(solve_day_03(input, false), "188741603");
+    }
+    #[test]
+    fn day_03_02_input() {
+        let input = include_str!("./input.txt");
+        assert_eq!(solve_day_03(input, true), "67269798");
     }
 }
